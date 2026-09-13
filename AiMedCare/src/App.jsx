690afe69c083433101315c7872439e815f87abcc
aniket-gaikwad30 <aniketgaikwadhere30@gmail.com>
2026-09-13 11:52:50 +0530

    changes

--- AiMedCare/src/App.jsx
+++ AiMedCare/src/App.jsx
@@ -11,6 +11,7 @@ import StarRating from "./pages/StarRating.jsx";
 import DietScreen from "./pages/DietScreen.jsx";
 import ReminderScreen from "./pages/ReminderScreen.jsx";
 import RiskScreen from "./pages/RiskScreen.jsx";
+import chatbot from "../src/ai/chatbot.jsx"
 
 export const App = () => {
   return (
@@ -51,6 +52,7 @@ export const App = () => {
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
+      <chatbot></chatbot>
     </div>
   );
 };
